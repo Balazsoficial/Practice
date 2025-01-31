61868ab722125497f9e5cfc745fee03b3b1aa92d
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-01-31 13:33:22 +0100

    initial commit

---
+++ wholenums.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int val1 {10};
+    int val2 {-300};
+    cout <<"val1: "<< val1 << endl;
+    cout <<"val2: "<< val2 << endl;
+    cout << "sizeof(val1): " << sizeof(val1) << endl;
+    cout << "sizeof(val2): " << sizeof(val2) << endl;
+
+    return 0;
+}
