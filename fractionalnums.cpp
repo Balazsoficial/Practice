41d9ed944d4e8256804294816acfbb2d8b07fef4
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-01-31 13:39:30 +0100

    initial commit

---
+++ fractionalnums.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Balazsoficial on 2025. 01. 31..
+//
+
+#include "fractionalnums.h"
--- wholenums.cpp
+++ wholenums.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int val1 {10};
+    short val1 {10};
     int val2 {-300};
+    long long int val3 {-400};
     cout <<"val1: "<< val1 << endl;
     cout <<"val2: "<< val2 << endl;
-    cout << "sizeof(val1): " << sizeof(val1) << endl;
-    cout << "sizeof(val2): " << sizeof(val2) << endl;
+    cout <<"val3: "<< val3 << endl;
+    cout << "(short) sizeof(val1): " << sizeof(val1) << endl;
+    cout << "(normal) sizeof(val2): " << sizeof(val2) << endl;
+    cout << "(long long) sizeof(val3): " << sizeof(val3) << endl;
+    //signed unsigned does not matter in memory consumption
 
     return 0;
 }
