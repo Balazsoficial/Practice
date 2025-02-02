1cde68e037b77075da1e586978853d0dfcb774ac
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-02-02 21:25:08 +0100

    Compound Operators

---
+++ compoundoperators.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Balazsoficial on 2025. 02. 02..
+//
+#include <iostream>
+using namespace std;
+int main(){
+      int val = 45;
+      cout << "val = " << val << endl;
+      val += 5;  //+5
+      cout << "val after addition of five = " << val << endl;
+      val -= 5;
+      cout << "val after subtraction of five = " << val << endl;
+      val *= 5;
+      cout << "val after multiplication of five = " << val << endl;
+      val /= 5;
+      cout << "val after division of five = " << val << endl;
+      val %= 5;
+      cout << "val after modulo of five = " << val << endl;
+
+  return 0;
+}
