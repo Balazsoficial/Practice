13a5820e4e4da4eafb2e8c9d1dc01bdf897ec372
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-02-09 20:58:11 +0100

    Playing with the limits library

---
+++ NumericLimits.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Balazsoficial on 2025. 02. 09..
+//
+#include <iostream>
+#include <limits>
+using namespace std;
+int main()
+    {
+    cout << "The range for short is from " << numeric_limits<short>::min() << " to " << numeric_limits<short>::max() << endl;
+    cout << "The range for unsigned short is from " << numeric_limits<unsigned short>::min() << " to " << numeric_limits<unsigned short>::max() << endl;
+    cout << "The range for int is from " << numeric_limits<int>::min() << " to " << numeric_limits<int>::max() << endl;
+    cout << "The range for unsigned int is from " << numeric_limits<unsigned int>::min() << " to " << numeric_limits<unsigned int>::max() << endl;
+    cout << "The range for long is from " << numeric_limits<long>::min() << " to " << numeric_limits<long>::max() << endl;
+    cout << "The range for float is from " << numeric_limits<float>::min() << " to " << numeric_limits<float>::max() << endl;
+    cout << "The range for double is from " << numeric_limits<double>::min() << " to " << numeric_limits<double>::max() << endl;
+    cout << "The range for long fouble is from " << numeric_limits<long double>::min() << " to " << numeric_limits<long double>::max() << endl;
+
+
+
+    }
