c3035f866a0b55ccba7f767893d3cf141f4a0cdf
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-02-16 21:36:50 +0100

    If operator

---
+++ if-operator.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Balazsoficial on 2025. 02. 16..
+//
+#include <iostream>
+using namespace std;
+int main() {
+    int number1 = 55;
+    int number2 = 60;
+
+    bool result = number1 < number2;
+    cout << boolalpha <<"result: "<< result << endl;
+
+
+    if (result == true) {
+      cout << number1 << " is less than " << number2 << endl;
+    }
+
+    if(!result==true) {
+      cout << number1 << " is not less than " << number2 << endl;
+    }
+
+    // with else
+    if (result == true) {
+      cout << number1 << " is less than " << number2 << endl;
+    }
+    else
+      cout << number1 << " is not less than " << number2 << endl;
+
+
+}
