df02ae0af361f30ea15092e05a2b9acdd6673763
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-02-25 22:04:22 +0100

    Ternary operators!

---
+++ ternary.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Balazsoficial on 2025. 02. 25..
+//
+#include <iostream>
+using namespace std;
+int main() {
+
+  int max;
+  int a =30;
+  int b=20;
+
+
+  max= (a>b)?a:b;
+
+
+  cout << "max is " << max << endl;
+
+
+
+
+}
