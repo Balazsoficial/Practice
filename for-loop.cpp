49370e7b2793bf0bdbb7e59a87b473e2c164da72
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-02-28 18:02:25 +0100

    For loops you can also manipulate the iterator from the inside

--- for-loop.cpp
+++ for-loop.cpp
@@ -4,6 +4,12 @@
 #include <iostream>
 using namespace std;
 int main() {
+    //std::size_t is the unsigned integer
+    for (size_t i ; i< 10 ; i++)
+    {
+        cout <<i  << "  Hello World" << endl;
 
-
+    }
+    cout << "Loop done" <<endl;
+    cout << "Size of size_t is : " << sizeof(size_t) << endl;
 }
