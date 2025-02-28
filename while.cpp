4acb93cc7ff6d477020193e7ac87f78dd70f103a
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-02-28 18:15:37 +0100

    while loop done

---
+++ while.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Balazsoficial on 2025. 02. 28..
+//
+#include <iostream>
+using namespace std;
+int main() {
+    size_t size = 10;
+    size_t i =0;
+    while(i < size) {
+      cout << i << "Hello world" << endl;
+      ++i;
+
+    }
+    cout << "loop done" <<endl;
+
+
+
+}
