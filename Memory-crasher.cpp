05d2a8918b00614a4077e2856214d393f911dc77
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-03-03 21:25:41 +0100

    Sometimes new instruction fails and leads to crashes

---
+++ Memory-crasher.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Balazsoficial on 2025. 03. 03..
+//
+#include <iostream>
+using namespace std;
+int main () {
+  for (size_t i =0; i<1000000;i++) {
+  try{
+    int * nums = new int[1000000];
+  }__catch(exception& ex){}}}
---
+++ pointer-fails.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Balazsoficial on 2025. 03. 03..
+//
+// times when the new operator fails
+#include <iostream>
+using namespace std;
+int main () {
+
+// we can use std:nothrow
+  for (size_t i =0; i<1000000;i++) {
+  try{
+
+    int * nums = new int[1000000];
+  }__catch(exception& ex){
+    cout << "Something went wrong: " << ex.what() << endl;
+  }
+
+  }
+  cout<< "Program ended well! " << endl;
+
+
+
+
+}
