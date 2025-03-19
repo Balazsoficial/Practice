ee7e4f3ca69b4135f249abc1f2b44fe8e7d7f3a9
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-03-19 22:03:27 +0100

    Capture lists

--- capture-all.cpp
+++ capture-all.cpp
@@ -4,5 +4,30 @@
 #include <iostream>
 using namespace std;
 int main(){
+    int c =42;
+    int d=5;
+        //capture all by value
+    auto func =[=]() {
+        cout << "Inner value: " << c << endl;
+    };
+
+    for (size_t i{};i<5;i++) {
+        cout << "Outer value: " << c << endl;
+        func();
+        ++c;
+    }
+    cout << endl;
+    //capture all by reference
+    auto func1 =[&]() {
+        cout << "Inner value: " << c << endl;
+        cout << "D: " << d << endl;
+    };
+
+    for (size_t i{};i<5;i++) {
+        cout << "Outer value: " << c << endl;
+        func1();
+        ++c;
+    }
+
 
 }
