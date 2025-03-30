67f8ee09a681d2b2a5087a5d971a50ca4da00184
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-03-30 18:48:31 +0200

    First class

---
+++ first-class.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Balazsoficial on 2025. 03. 30..
+//
+#include <iostream>
+#include <cmath>
+using namespace std;
+const double pi = numbers::pi;
+class Cylinder {
+    public:
+        double volume() {
+            return pi*base_radius*base_radius*base_height;
+        }
+//private:    //this can not be accsesed from outside  the class
+    public:
+        double base_radius{};
+        double base_height{};
+    };
+int main() {
+    Cylinder cylinder1;
+
+    cout <<"volume: " <<  cylinder1.volume() << endl;
+    cylinder1.base_radius = 10;
+    cylinder1.base_height = 20;
+    cout <<"After change ---->"<<endl;
+
+    cout <<"volume: " <<  cylinder1.volume() << endl;
+
+
+
+
+}
