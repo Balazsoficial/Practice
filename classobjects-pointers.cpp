b37a0743f8eda873b13fc44362a9e243b2e50fc2
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-04-02 22:19:36 +0200

    destructors

--- classobjects-pointers.cpp
+++ classobjects-pointers.cpp
@@ -8,11 +8,12 @@ int main() {
 
     Cylinder c1 = Cylinder(11,20);
     cout << "Volume of c1: " << c1.volume() << endl;
+    Cylinder * c3 =&c1; //can initialize like this using another ones adress
 
-    Cylinder *c2 = new Cylinder(12,20);
+    Cylinder *c2 = new Cylinder(12,20);  //creates a heap object
     cout << "Volume of c2: " << c2->volume() << endl;       //can be used instead of dereferencing
     cout << "Volume of c2: " <<(*c2).volume() << endl;   //dereferencing is required
-    delete c2;
+    delete c2;  //release the memory
 
 
 
---
+++ destructors.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Balazsoficial on 2025. 04. 02..
+//
+#include<iostream>
+class Dog {
+    public:
+
+    Dog(std::string_view name_param,std::string_view breed_param, int age_param);
+    ~Dog();
+
+    private:
+    std::string name;
+    std::string breed;
+    int * p_age{nullptr};
+
+
+};
+Dog:: Dog(std::string_view name_param,std::string_view breed_param, int age_param) {
+    name=name_param;
+    breed=breed_param;
+    p_age=new int;
+    *p_age=age_param;
+    std::cout <<"Dog constructor called for : "<< name<<std::endl;
+}
+Dog::~Dog() {
+    delete p_age;
+    std::cout <<"Dog destructor called for : "<< name<<std::endl;
+}
+void something() {
+    Dog my_dog("Dawg","sheperd",2);
+}
+
+int main() {
+    something();
+    std::cout << "done"<<std::endl;
+    return 0;
+
+
+}
