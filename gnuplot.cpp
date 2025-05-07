333677c155ee78d8a0a7bcabbab93cd443573d80
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-05-07 22:17:09 +0200

    mostly unnecesarry code and the run window cant handle it

--- gnuplot.cpp
+++ gnuplot.cpp
@@ -10,12 +10,12 @@ int main() {
 "unset surface;"
 "set cntrparam levels discrete 0;"
 "set view map;"
-"set xtics axis;"
+/*"set xtics axis;"
 "set ytics axis;"
 "set arrow 1 from 0,0 to graph 1, first 0 filled head;"
 "set arrow 2 from 0,0 to first 0, graph 1 filled head;"
 "set arrow 3 from 0,0 to graph 0, first 0 filled head;"
-"set arrow 4 from 0,0 to graph 0, first 0 filled head;"
+"set arrow 4 from 0,0 to graph 0, first 0 filled head;"*/
     "plot sin(x)\" -p";
     ShellExecute(0, "open", "gnuplot.exe", builder.c_str(), 0, SW_MINIMIZE);
 
