74c40e75adb3f4aa83d07e5e0aac37b8713bd246
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-05-12 18:07:12 +0200

    backtracking written for rooks

--- bastyak.cpp
+++ bastyak.cpp
@@ -5,13 +5,13 @@
 
 using namespace std;
 int db=0;
-int tabla[8];
+int sor[8];
 int bastya=9;
 void backtrack(int szint);
 int main() {
 
-    for (int i=0; i<=size(tabla);i++) {
-        tabla[i]=0;
+    for (int i=0; i<=size(sor);i++) {
+        sor[i]=0;
     }
     backtrack(1);
     cout << db<< endl;
@@ -22,16 +22,16 @@ void backtrack(int szint) {
     if (bastya==szint) {
         db++;
         for (int i=1; i<=8; i++) {
-            cout << tabla[i] << " ";
+            cout << sor[i] << " ";
         }
         cout << endl;
     }
     else {
         for (int i=1; i<=8;i++) {
-            if (tabla[i]==0) {
-                tabla[i]=szint;
+            if (sor[i]==0) {
+                sor[i]=szint;
                 backtrack(szint+1);
-                tabla[i] =0;
+                sor[i] =0;
             }
 
         }
