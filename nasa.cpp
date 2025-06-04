03f83afc159fac97de2b09553a913f93fd839ab7
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-06-04 20:41:03 +0200

    cpr fixed and nasa random bck downloader

--- nasa.cpp
+++ nasa.cpp
@@ -3,6 +3,39 @@
 //
 #include <iostream>
 #include <cpr/cpr.h>
-int main() {
+#include <nlohmann/json.hpp>
+#include <filesystem>
+using json = nlohmann::json;
+using namespace  std;
+int main(){
+
+                    //  cpr::Parameters{{"hello", "world"}});
+
+    //std::cout << r.url << std::endl; // http://www.httpbin.org/get?hello=world
+   // std::cout << r.text << std::endl;
+    int iterations;
+    string apikey;
+    cout << "Api kulcs:" ;
+    cin >> apikey;
+    cout << "hany kep kell?" << endl;
+    cin >> iterations;
+    for (int i = 0; i < iterations; i++) {
+
+        cpr::Response r = cpr::Get(cpr::Url{"https://api.nasa.gov/planetary/apod?count=1&api_key="+apikey});
+
+        filesystem::create_directory("D:/download/nasa");
+        nlohmann::json j = json::parse(r.text);
+        std::ofstream of("D:/download/nasa/"+to_string(i)+".jpg", std::ios::binary);
+        cpr::Response x = cpr::Download(of, cpr::Url{j[0]["hdurl"]});
+        if (x.status_code != 200) {
+            cout << "There was an issue while downloading" << endl;
+            _sleep(1000);
+        }
+
+        std::cout << "http status code = " << x.status_code << std::endl << std::endl;
+
+    }
+
+
 
 }
