6ad7e6f751ed301306b0c675882e60c91ce463e8
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-06-10 22:04:58 +0200

    Exe not being embeded for some reason

--- nasahelper.cpp
+++ nasahelper.cpp
@@ -3,13 +3,28 @@
 //
 
 #include "nasahelper.h"
+#include <conio.h>
+#include <windows.h>
+#include <iostream>
+#include <bits/ostream.tcc>
+#include <fstream>
+#define IDB_EMBEDEXE 101
+using namespace std;
+int main() {
+    HRSRC hResource = FindResource(NULL, MAKEINTRESOURCE(IDB_EMBEDEXE), TEXT("BINARY"));
+    HGLOBAL hGlobal = LoadResource(NULL, hResource);
+    size_t exeSiz = SizeofResource(NULL, hResource); // Size of the embedded data
+    void*  exeBuf = LockResource(hGlobal);
+    if (!hResource)
+        cout  << "Couldnt lock resource" << endl;
 
-#include <cpr/api.h>
-#define IDB_EMBEDEXE
 
-/*
-HRSRC hResource = FindResource(NULL, MAKEINTRESOURCE(IDB_EMBEDEXE), _TEXT("BINARY"));
-HGLOBAL hGlobal = LoadResource(NULL, hResource);
-size_t exeSiz = SizeofResource(NULL, hResource); // Size of the embedded data
-void*  exeBuf = LockResource(hGlobal);
-*/
+    cout << "asd" << endl;
+    cout << exeSiz;
+   hResource;
+    std::ofstream of("asd.exe", std::ios::binary);
+    cout << exeBuf;
+    of << exeBuf;
+
+}
+
--- nasahelper.h
+++ nasahelper.h
@@ -1,6 +1,5 @@
 //
 // Created by Balazsoficial on 2025. 06. 08..
 //
-/*
-IDB_EMBEDEXE    BINARY      "<path>\\practice.exe"
-*/
+
+#define IDB_EMBEDEXE 101
