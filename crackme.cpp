c23d0a4ae7a79e2ec44cb505b3621c24d2ce9a40
Balazsoficial <193341121+Balazsoficial@users.noreply.github.com>
2025-07-10 00:36:37 +0200

    Txrbos crackme but with only the useful code in cpp

---
+++ crackme.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+bool Length_check(string input) {
+    if (input.length() >= 8 && input.length()<=16) {
+        return true;
+    }
+    else{return false;}
+
+
+}
+bool Lower_check(string input) {
+    bool rt=false;
+    for (auto c : input) {
+        if (c==tolower(c)) {
+            rt=true;
+        }
+    }
+    return rt;
+
+}
+bool Upper_check(string input) {
+    bool rt=false;
+    for (auto c : input) {
+        if (c==toupper(c)) {
+            rt=true;
+        }
+    }
+   return rt;
+}
+bool Main_check(string input) {
+    if (Length_check(input) && Lower_check(input) && Upper_check(input)) {
+        return true;
+    }
+    else{return false;}
+}
+
+int main() {
+    while (1==1){        string password;
+        cin >> password;
+        if (Main_check(password)) {
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }}
+
+
+
+
+
+}
